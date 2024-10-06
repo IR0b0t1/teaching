9fe9bc493e63efb2f1bf24c8427df37e6fe9daa7
AlgorithmMstr <flitewka@gmail.com>
2024-10-06 13:16:26 +0200

    Dodanie routera

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,17 @@
 <script>
+  import Router from "svelte-spa-router";
+  import { routes } from "./routes.js";
   import Header from "./components/header.svelte";
+  import Footer from "./components/footer.svelte";
 </script>
 
 <Header />
 
-<main></main>
+<main class="min-h-screen bg-gray-700">
+  <Router {routes} />
+</main>
+
+<Footer />
 
 <style>
 </style>
---
+++ src/routes.js
@@ -0,0 +1,14 @@
+// import Home from "./routes/Home.svelte";
+// import NotFound from "./routes/NotFound.svelte";
+// import Countries from "./routes/Countries.svelte";
+// import ColorPicker from "./routes/ColorPicker.svelte";
+// import ChuckNorris from "./routes/ChuckNorris.svelte";
+
+export const routes = {
+  //   "/": Home,
+  //   "/notfound": NotFound,
+  //   "/countries": Countries,
+  //   "/colorpicker": ColorPicker,
+  //   "/chucknorris": ChuckNorris,
+  //   "*": NotFound,
+};
