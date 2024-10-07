0bec5f3f0bd20a75df75504f5c9933ba4b759d4a
AlgorithmMstr <flitewka@gmail.com>
2024-10-07 19:33:52 +0200

    Dodano Home.svelte, NotFound.svelte, Countries.svelte, ColorPicker.svelte, zaktualizowano scierzki i poprawiono problemy z tailwindcss

---
+++ src/routes/Home.svelte
@@ -0,0 +1,7 @@
+<script>
+  let message = "Welcome to the home page";
+</script>
+
+<main class="min-h-screen flex items-center justify-center">
+  <h1 class="text-4xl font-bold text-white">{message}</h1>
+</main>
---
+++ src/routes/NotFound.svelte
@@ -0,0 +1,23 @@
+<script>
+  let message = "Oops! Page not found.";
+</script>
+
+<main
+  class="min-h-screen flex flex-col items-center justify-center text-center"
+>
+  <h1 class="text-4xl font-bold text-black-800">{message}</h1>
+  <p class="text-lg mt-4 text-black-600">
+    The page you are looking for doesn't exist.
+  </p>
+  <a
+    href="#/"
+    class="mt-6 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+    >Go Back Home</a
+  >
+</main>
+
+<style>
+  main {
+    padding: 2rem;
+  }
+</style>
